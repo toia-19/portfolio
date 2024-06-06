609934909936464c54257735fb6117d5c0eb25b2
toia-19 <vickingiediaz19@gmail.com>
2024-06-05 23:54:46 -0300

    agregue un componente de material-ui para la sección del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-// componentes globales
+// Componentes globales
 import Navbar from './shared/navbar';
 import Footer from './shared/footer';
 
-// bloques para la portada
+// Bloques para la portada
 import Personaldata from './pages/personaldata';
 import Skills from './pages/skills';
 import Techstack from './pages/techstack';
@@ -14,15 +14,17 @@ import Hobbies from './pages/hobbies';
 function App() {
   return (
     <>
-      <Navbar/>
-      <img src="src\assets\avatar4.png" alt="Foto de avatar" className="logo"/>
+      <Navbar />
+      <img src="src\assets\avatar4.png" alt="Foto de avatar" className="logo" />
+
       <div className="container">
-        <Personaldata/>
-        <Skills/>
-        <Techstack/>
-        <Hobbies/>
+        <Personaldata />
+        <Skills />
+        <Techstack />
+        <Hobbies />
       </div>
-      <Footer/>
+
+      <Footer />
     </>
   )
 }
--- src/shared/footer.jsx
+++ src/shared/footer.jsx
@@ -1,17 +1,54 @@
 import React from "react";
 import './style.css';
 
-export default function Footer(){
+// Componentes Material-UI
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+export default function Footer() {
+    // Corresponde al botón y ventana emergente de Dialog
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <>
             <footer className="crumbs">
                 <ol>
                     <li className="crumb"><a href="">Inicio</a></li>
-                    <li className="crumb"><a href="">Contacto</a></li>
                     <li className="crumb"><a href="">Más Información</a></li>
+                    <Button variant="text" color="secondary" size="small" onClick={handleClickOpen}>
+                        Contacto
+                    </Button>
                     <li className="crumb">Copyright © 2024 RN, Argentina</li>
                 </ol>
             </footer>
+
+
+            {/* Diálogo para ver el contacto */}
+            <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description" >
+
+                <DialogTitle id="alert-dialog-title">
+                    {"Contactame :)"}
+                </DialogTitle>
+
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <a href="https://github.com/toia-19">GitHub</a> <br />
+                        <a href="https://www.linkedin.com/in/victoria-ang%C3%A9lina-diaz/">LinkedIn</a>
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
         </>
     )
 }
